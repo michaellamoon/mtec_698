1d49da6355f8c93fda15ed21219f3693a609b518
michaellamoon <michaella@Michaellas-MBP.attlocal.net>
2022-02-13 13:32:07 -0800

    week 3 update

---
+++ week3/Source/PluginEditor.cpp
@@ -0,0 +1,52 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+Week3AudioProcessorEditor::Week3AudioProcessorEditor (Week3AudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    //-----SLIDER1-------------------
+    mSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    mSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 150, 20);
+    mSlider.setRange(0.f, 1.f);
+    addAndMakeVisible(mSlider);
+    
+    mSlider.onValueChange = [this]() {
+        audioProcessor.setSineVolume(mSlider.getValue());
+        
+    //update value of slider
+    mSlider.setValue(audioProcessor.getSineVolume());
+            
+    setSize(400, 300);
+    };
+}
+
+Week3AudioProcessorEditor::~Week3AudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void Week3AudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+/*
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+ */
+}
+
+void Week3AudioProcessorEditor::resized()
+{
+    // silder1 position---------------
+    mSlider.setBounds(0, 0, 150, 150);
+}
---
+++ week3/Source/SineWave.cpp
@@ -0,0 +1,43 @@
+//
+//  SineWave.cpp
+//  week3
+//
+//  Created by michaella on 2/13/22.
+//
+
+#include "SineWave.h"
+
+//CONSTRUCTOR
+SineWave::SineWave() {}
+//DECONSTRUCTOR
+SineWave::~SineWave() {}
+
+//INITALIZE
+void SineWave::initialize(float inFrequencyHz, float inSampleRate) {
+    
+    // Set frequency (HZ)
+    mFreqHz = inFrequencyHz;
+    
+    // Set samplerate
+    mSampleRate = inSampleRate;
+}
+
+//GETNEXTSAMPLE
+float SineWave::getNextSample() {
+    
+    // calculate our sine function output
+    //-- using a sin call scaled to repeat between 0 - 1 on the input
+    float output = std::sin(juce::MathConstants<float>::twoPi * mPhase);
+    
+    // move our phase forward in the sign table by a single step
+    //-- determined by our desired samplerate & playback hz
+    mPhase += mFreqHz / mSampleRate;
+    
+    // if we go passed 1 -- lets loop back around the sine wave
+    if (mPhase > 1.f) {
+        mPhase -= 1.f;
+    }
+    
+    // return the output to the caller
+    return output;
+}
